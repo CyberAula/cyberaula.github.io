96db9222074dbf72863ed4541bfdcf2d4d042d6b
unknown <thepikahouse@gmail.com>
2024-05-14 12:39:04 +0200

    recent publications cambiado

--- components/RecentPublications.js
+++ components/RecentPublications.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import { mypublications } from '../constants/publications';
+import React from "react";
+import { useState, useEffect } from "react";
+import { mypublications } from "../constants/publications";
 
-export default function RecentPublications (props) {
-    const [papers, setPapers] = useState(mypublications);
+export default function RecentPublications(props) {
+  const [papers, setPapers] = useState(mypublications);
 
-    useEffect(() => {
-		window.scrollTo(0,0);		
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-        return <div className="_pubications _recent_publications">
-            { papers.filter(paper => {
-                    return (
-                        paper.type === "article-journal"
-                    );
-                })
-                .filter((i, index) => (index < 3))
-                .map(({doi, content}) => {
-                    return (
-                        <a key={doi} href={doi} target="_blank" rel="noopener noreferrer">
-                            <p style={{ color: "black" }}dangerouslySetInnerHTML={{__html: content}}></p>
-                        </a>
-                     );
-                 })}
-            </div>    
-
+  return (
+    <div className="_pubications _recent_publications">
+      {papers
+        .filter((paper) => {
+          return paper.type === "article-journal";
+        })
+        .filter((i, index) => index < 3)
+        .map(({ doi,title,author,date, content }) => {
+          return (
+            <a key={doi} href={doi} target="_blank" rel="noopener noreferrer">
+              <div className="article">
+                <div className="article_border"></div>
+                <div className="article_content">
+                <div className="article_top"><span>{date}</span></div>
+                <h5 className="article_title">{title}</h5>
+                <div className="article_author">{author}</div>
+                <div className="article_doi">{doi}</div>
+                </div>
+              </div>
+            </a>
+          );
+        })}
+    </div>
+  );
 }
