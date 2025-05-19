c1c6a827ee7583c9f54b2d94b863a1b514392f0f
june <belencastillocha@gmail.com>
2025-05-19 11:27:06 +0200

    objetivos

--- components/gridElement.js
+++ components/gridElement.js
@@ -7,29 +7,29 @@ export default function GridElement(props) {
   return (
     <div className="grid_container">
       {" "}
-      <div className="grid_item">
+      <div className="grid_item orange_gradient">
         <h5>{t("front.gridElements.topL")}</h5>
         <small className="small_sub"></small>
       </div>
-      <div className="grid_item">
+      <div className="grid_item darkblue">
         <h5>{t("front.gridElements.topM")}</h5>
         <small className="small_sub">{t("front.gridElements.topMsub")}</small>
       </div>
-      <div className="grid_item">
+      <div className="grid_item ">
         <h5>{t("front.gridElements.topR")}</h5>
         <small className="small_sub"></small>
       </div>
-      <div className="grid_item">
+      <div className="grid_item purple_gradient">
         <h5>{t("front.gridElements.bottomL")}</h5>
         <small className="small_sub"></small>
       </div>
-      <div className="grid_item">
+      <div className="grid_item lightblue">
         <h5>{t("front.gridElements.bottomM")}</h5>
         <small className="small_sub">
           {t("front.gridElements.bottomMsub")}
         </small>
       </div>
-      <div className="grid_item bottom_right">
+      <div className="grid_item orange_gradient">
         <h5>{t("front.gridElements.bottomR")} </h5>
         <small className="small_sub"></small>
       </div>
